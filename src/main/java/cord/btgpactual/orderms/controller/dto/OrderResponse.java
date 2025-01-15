b195cbacbaf92aacbd7b28d790d8797fdae1a0fa
JulioCezarbc <julio04.cordeiro@gmail.com>
2025-01-15 20:57:28 -0300

    feat: Criando os DTOs necessarios

---
+++ src/main/java/cord/btgpactual/orderms/controller/dto/OrderResponse.java
@@ -0,0 +1,14 @@
+package cord.btgpactual.orderms.controller.dto;
+
+import cord.btgpactual.orderms.entity.Order;
+
+import java.math.BigDecimal;
+
+public record OrderResponse(Long orderId,
+                            Long customerId,
+                            BigDecimal total) {
+
+    public static OrderResponse fromEntity(Order order){
+        return new OrderResponse(order.getOrderId(), order.getCustomerId(), order.getTotal());
+    }
+}
---
+++ src/main/java/cord/btgpactual/orderms/listener/dto/OrderItemEvent.java
@@ -0,0 +1,8 @@
+package cord.btgpactual.orderms.listener.dto;
+
+import java.math.BigDecimal;
+
+public record OrderItemEvent(String produto,
+                             Integer quantidade,
+                             BigDecimal preco) {
+}
